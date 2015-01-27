02eb0861d35f6e21b8c568b795c57f3471342d78
Steve Witman <stevewitman@gmail.com>
2015-01-27 14:30:56 -0700

    added flash messages

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -3,23 +3,26 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
-      var body = this.get('noteCopy');
-      var title = this.get('noteTitle');
+      var body = this.get('noteCopy') || '';
+      var title = this.get('noteTitle') || '';
       if (body.trim() && title.trim()) {
         var note = this.store.createRecord('note', { body: body, title: title });
         this.set('noteTitle', '');
         this.set('noteCopy', '');
         note.save();
-      } 
+        this.flashMessage("", "Note was saved.");
+      } else if (!title.trim()) {
+        this.flashMessage("", "Title can't be blank.");
+      } else {
+        this.flashMessage("", "Body can't be blank.");
+      }
     },
     deleteNote: function(note_id) {
-      var note = this.store.find('note', note_id).then(function(note) {
+      this.store.find('note', note_id).then((function(note) {
         note.deleteRecord();
         note.save();
-      });
+        this.flashMessage("", "Note was deleted.");
+      }).bind(this))
     },
-    formSubmitted: function() {
-      this.flashMessage('success', 'Congratulations! Your changes have been saved');
-    }
   }
 });
