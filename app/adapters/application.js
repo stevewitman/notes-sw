6c0c9b377613beaed683ae87f8be6750ab8638d5
Steve Witman <stevewitman@gmail.com>
2015-01-28 11:12:13 -0700

    direct storage to rails api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,7 @@
 import DS from "ember-data";
 
-export default DS.LSAdapter.extend({});
+// export default DS.LSAdapter.extend({}); This stored data on local browser storage.
+
+export default DS.ActiveModelAdapter.extend({
+  host: 'http://localhost:3000'
+});
