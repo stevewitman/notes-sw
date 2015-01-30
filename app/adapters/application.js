cef584f7e7db4080f7ee53d3f8c5df13e81f4077
Steve Witman <stevewitman@gmail.com>
2015-01-30 15:10:06 -0700

    point adapter to production api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import DS from "ember-data";
 // export default DS.LSAdapter.extend({}); This stored data on local browser storage.
 
 export default DS.ActiveModelAdapter.extend({
-  // host: 'https://notes-api-sw.herokuapp.com'
-  host: 'http://localhost:3000'
+  host: 'https://notes-api-sw.herokuapp.com'
+  // host: 'http://localhost:3000'
 });
