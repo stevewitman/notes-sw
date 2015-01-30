8145fa8e9d03a44ea37e57985d71c6531af6aae0
Steve Witman <stevewitman@gmail.com>
2015-01-30 06:31:54 -0700

    pointed to rails api on heroku

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,5 @@ import DS from "ember-data";
 // export default DS.LSAdapter.extend({}); This stored data on local browser storage.
 
 export default DS.ActiveModelAdapter.extend({
-  host: 'http://localhost:3000'
+  host: 'http://notes-api-sw.herokuapp.com/'
 });
--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self'",
       'font-src': "'self'",
-      'connect-src': "'self' localhost:3000",
+      'connect-src': "'self' //notes-api-sw.herokuapp.com/",
       'img-src': "'self'",
       'style-src': "'self'",
       'media-src': "'self'"
